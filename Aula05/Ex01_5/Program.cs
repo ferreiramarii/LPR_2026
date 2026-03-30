9343ac3b13bfeac4406819365debf37e6a6297e9
Mariana <marianafdasilva5@gmail.com>
2026-03-29 22:09:30 -0300

    commit05

--- Aula05/Ex01_5/Program.cs
+++ Aula05/Ex01_5/Program.cs
@@ -1,6 +1,19 @@
-﻿List<int> numeros = new List<int>{};
+﻿int soma = 0;
+int pares = 0;
+int i = 1;
 
-Console.WriteLine("Digite o número de valores digitados e o conjunto de números: ");
-numeros = Console.ReadLine();
+Console.WriteLine("Digite a quantidade de números (primeiro dígito) e os números em seguida");
+string[] valores = Console.ReadLine().Split(' ')?? new string[0];
+int quantidade = int.Parse(valores[0]);
 
+while(i <= quantidade){
+    int numero = int.Parse(valores[i]);
 
+    if(numero % 2 == 0){
+        soma += numero;
+        pares++;
+    }
+    i++;
+}
+  double media = (double)soma / pares;
+  Console.WriteLine($"A média é: {media}");
---
+++ Aula05/Ex02_5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <random>
+using namespace std;
+
+
+int main(){
+    random_device numAleatorio ;
+    uniform_int_distribution<> distribuicao(1,100);
+    int valorInteiro = distribuicao(numAleatorio);
+
+    int numero;
+    int tentativas = 0;
+do {
+        cout << "Digite seu chute: ";
+        cin >> chute;
+        tentativas++;
+
+        if (numero > valorInteiro) {
+            cout << "Chutou alto! Tente novamente." << endl;
+        } 
+        else if (numero < valorInteiro) {
+            cout << "Chutou baixo! Tente novamente." << endl;
+        } 
+        else {
+            cout << "Parabens! Voce acertou!" << endl;
+        }
+
+    } while (numero != valorInteiro); 
+
+    cout << tentativas << " tentativas para vencer." << endl;
+
+    return 0;
+}
